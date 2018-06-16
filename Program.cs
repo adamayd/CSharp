9f4f88b45094b5f67f1fbd58a9d14fb37f616aa1
Adam Ayd <adamayd@gmail.com>
2018-06-16 10:57:26 -0400

    Cleaned up interface

--- Program.cs
+++ Program.cs
@@ -15,9 +15,6 @@ namespace NumberGuesser
 
             while (true)
             {
-                // set random number
-                //int correctNumber = 7;
-
                 // create a new random object
                 Random randomNum = new Random();
 
@@ -28,7 +25,7 @@ namespace NumberGuesser
                 int guess = 0;
 
                 // write instrutions
-                Console.WriteLine("Guess a number between 1 and 10");
+                Console.Write("Guess a number between 1 and 10: ");
 
                 // while guess is not correct
                 while (guess != correctNumber)
@@ -40,7 +37,7 @@ namespace NumberGuesser
                     if (!int.TryParse(guessInput, out guess))
                     {
                         //print error message
-                        PrintColorMessage(ConsoleColor.Red, guessInput + " is not a number");
+                        PrintColorMessage(ConsoleColor.Red, guessInput + " is not a number, Guess Again : ", false);
 
                         // keep going after error
                         continue;
@@ -53,15 +50,15 @@ namespace NumberGuesser
                     if (guess != correctNumber)
                     {
                         //write out incorrect number
-                        PrintColorMessage(ConsoleColor.Red, "Incorrect Number, Guess Again");
+                        PrintColorMessage(ConsoleColor.Red, "Incorrect Number, Guess Again : ", false);
                     }
                 }
 
                 // output success message
-                PrintColorMessage(ConsoleColor.Yellow, "You guessed the correct number of " + correctNumber + ", Way to go " + inputName);
+                PrintColorMessage(ConsoleColor.Green, "You guessed the correct number of " + correctNumber + ", Way to go " + inputName, true);
 
                 // ask to play again
-                Console.WriteLine("Play again? (y/n)");
+                Console.Write("Play again? (y/n) ");
 
                 // get the answer
                 string answer = Console.ReadLine().ToUpper();
@@ -84,7 +81,7 @@ namespace NumberGuesser
             string appAuthor = "Adam Ayd";
 
             // change text color
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Yellow;
 
             // Write out app info
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
@@ -97,7 +94,7 @@ namespace NumberGuesser
         static string GreetUser()
         {
             // ask users name
-            Console.WriteLine("What is your name?");
+            Console.Write("What is your name? : ");
 
             // get user input
             string inputName = Console.ReadLine();
@@ -109,13 +106,22 @@ namespace NumberGuesser
         }
 
         // print color message 
-        static void PrintColorMessage(ConsoleColor color, string message)
+        static void PrintColorMessage(ConsoleColor color, string message, bool fullLine)
         {
             // change text color
             Console.ForegroundColor = color;
 
-            // write out success message
-            Console.WriteLine(message);
+            // determine to write or writeline
+            if(fullLine)
+            {
+                // write out success message
+                Console.WriteLine(message);
+            }
+            else
+            {
+                //write out message on same line
+                Console.Write(message);
+            }
 
             // reset the console color
             Console.ResetColor();
