600e2b4c5c4fede1ff5ddd6347e395987e17d32d
Adam Ayd <adamayd@gmail.com>
2018-06-17 23:06:57 -0400

    Chapter 2-1 complete

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace FirstConsoleApp
 {
     class Program
     {
+        public static string title = "C# Essential Training";
+        public static int whichPart;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C* Essential Training Part 1");
-            Console.WriteLine("Press enter to continue");
+            whichPart = 2;
+            var message = string.Format("Welcome to {0} Part {1}!", title, whichPart);
+            Console.WriteLine(message);
+            Console.WriteLine("Press the enter key to exit.");
             Console.ReadLine();
         }
     }
