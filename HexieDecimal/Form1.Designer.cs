dea7cfcc871458a8a83e0d3ba7d206df4dcf0868
Adam Ayd <adamayd@gmail.com>
2018-06-26 22:19:23 -0400

    Basic fom complete

--- HexieDecimal/Form1.Designer.cs
+++ HexieDecimal/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.hexPEG = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.decPEG = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Event Code";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.hexPEG);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(37, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(341, 133);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hexadecimal";
+            // 
+            // hexPEG
+            // 
+            this.hexPEG.Location = new System.Drawing.Point(21, 57);
+            this.hexPEG.Name = "hexPEG";
+            this.hexPEG.Size = new System.Drawing.Size(294, 22);
+            this.hexPEG.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.decPEG);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(422, 38);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(341, 133);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Decimal";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // decPEG
+            // 
+            this.decPEG.Location = new System.Drawing.Point(21, 57);
+            this.decPEG.Name = "decPEG";
+            this.decPEG.Size = new System.Drawing.Size(294, 22);
+            this.decPEG.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Event Code";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox hexPEG;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox decPEG;
+        private System.Windows.Forms.Label label2;
     }
 }
 
