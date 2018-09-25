b3e9d1532357d4c1c9d01f9f04195d47daaaaad0
Adam Ayd <adam.ayd@gmail.com>
2018-09-25 13:19:12 -0400

    created school class

---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string SchoolName;
+        string SchoolAddress;
+        string SchoolCity;
+        string SchoolState;
+        string SchoolZip;
+        string PhoneNumber;
+
+    }
+}
