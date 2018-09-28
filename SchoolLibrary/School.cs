8fbad8315c9a8213e0cd408e4bd01948c67d213b
Adam Ayd <adam.ayd@gmail.com>
2018-09-27 23:14:08 -0400

    Chapter 4 Part 1 Complete

--- SchoolLibrary/School.cs
+++ SchoolLibrary/School.cs
@@ -4,14 +4,70 @@ using System.Text;
 
 namespace SchoolLibrary
 {
-    class School
+    public class School
     {
-        string SchoolName;
-        string SchoolAddress;
-        string SchoolCity;
-        string SchoolState;
-        string SchoolZip;
-        string PhoneNumber;
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
 
+        private string _twitterAddress;
+        public string TwitterAddress
+        {
+            // make sure twitter address starts with @
+            get { return _twitterAddress; }
+            set
+            {
+                if (value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                } else
+                {
+                    throw new Exception("The twitter address must begin with @");
+                }
+            }
+        }
+
+        public School()
+        {
+            Name = "Untitled School";
+            PhoneNumber = "555-1234";
+        }
+
+        public School(string SchoolName, string SchoolPhoneNumber)
+        {
+            Name = SchoolName;
+            PhoneNumber = SchoolPhoneNumber;
+        }
+
+        //public float AverageThreeScores(float a, float b, float c)
+        //{
+        //    var result = (a + b + c) / 3;
+        //    return result;
+        //}
+
+        public static float AverageThreeScores(float a, float b, float c) => (a + b + c) / 3;
+
+        public static int AverageThreeScores(int a, int b, int c)
+        {
+            int result = (a + b + c) / 3;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(Name);
+            sb.AppendLine(Address);
+            sb.Append(City);
+            sb.Append(", ");
+            sb.Append(State);
+            sb.Append("  ");
+            sb.AppendLine(Zip);
+
+            return sb.ToString();
+        }
     }
 }
