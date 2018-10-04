80bf11b63575cc1e0e4783cdf429f14b10ce6d98
Adam Ayd <adam.ayd@gmail.com>
2018-10-04 13:10:48 -0400

    Part 2 Chapter 4 Complete

--- EssentialTrainingApp/Program.cs
+++ EssentialTrainingApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace EssentialTrainingApp
 {
@@ -17,6 +18,36 @@ namespace EssentialTrainingApp
             Words.Add("Cheese");
 
             CrazyMathProblem();
+            ReadTextFile();
+            Console.ReadLine();
+        }
+
+        private static void ReadTextFile()
+        {
+            try
+            {
+                using (var sr = new StreamReader(@"C:\temp\test.txt"))
+                {
+                    string contents = sr.ReadToEnd();
+                    Console.WriteLine(contents);
+                }
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Could not find the directory");
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("Could not find the file");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("An unknown error occured " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("The finally runs all the time as well as the catch");
+            }
         }
 
         private static int CrazyMathProblem()
