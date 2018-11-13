af6081acef59d3f18e665f21cfb4e111bd6cf9d8
Adam Ayd <adam.ayd@gmail.com>
2018-11-12 23:55:54 -0500

    completed passcode challenge

---
+++ Learning_CSharp/Passcode/Passcode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the passcode: ");
+            var passcode = Console.ReadLine();
+
+            if (passcode.ToLower() == "secret")
+            {
+                Console.WriteLine("You are authenticated");
+            }
+            else if (passcode.ToLower() != "secret")
+            {
+                Console.WriteLine("Incorrect passcode");
+            }
+        }
+    }
+}
