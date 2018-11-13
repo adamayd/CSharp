1a0be509e01bff17e30a87454213a094f5ba063b
Adam Ayd <adam.ayd@gmail.com>
2018-11-12 23:31:48 -0500

    Completed survey code challenge

---
+++ Learning_CSharp/Survey/Survey/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+
+            Console.Write("How old are you? ");
+            var age = Console.ReadLine();
+
+            Console.Write("What month were you born in? ");
+            var birthMonth = Console.ReadLine();
+
+            Console.WriteLine("{0} is {1} years old and was born in the month of {2}", name, age, birthMonth);
+        }
+    }
+}
