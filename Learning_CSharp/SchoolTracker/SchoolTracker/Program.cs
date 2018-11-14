66d26ef5fc72df729b79669193e7ecb482b359be
Adam Ayd <adam.ayd@gmail.com>
2018-11-13 22:39:27 -0500

    added array and foreach loops

---
+++ Learning_CSharp/SchoolTracker/SchoolTracker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrades = new int[] { 80, 77, 92, 68, 83, 85, 81, 79, 96, 100 };
+
+            foreach (var student in studentGrades)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
