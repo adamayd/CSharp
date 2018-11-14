9d88010228269e9e60dc9ae10c16e88d861629d2
Adam Ayd <adam.ayd@gmail.com>
2018-11-13 22:29:53 -0500

    completed nested loop or loop counting challenge

---
+++ Learning_CSharp/NestedLoopChallenge/NestedLoopChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NestedLoopChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write(j + " ");
+                }
+
+                for (int j = 10; j != 0; j--)
+                {
+                    Console.Write(j + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
