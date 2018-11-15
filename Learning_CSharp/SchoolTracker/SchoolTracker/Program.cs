17b0bf45af3711b9f8990f483ad5c528ddc19f50
Adam Ayd <adam.ayd@gmail.com>
2018-11-14 22:02:18 -0500

    created student class and replaced lists with list of objects

--- Learning_CSharp/SchoolTracker/SchoolTracker/Program.cs
+++ Learning_CSharp/SchoolTracker/SchoolTracker/Program.cs
@@ -10,18 +10,34 @@ namespace SchoolTracker
             //Console.Write("How many students in your class? ");
             //var studentCount = int.Parse(Console.ReadLine());
 
-            var studentNames = new List<string>();
-            var studentGrades = new List<int>();
+            var students = new List<Student>();
+
+            // replaced Lists below with List of Objects above
+            //var studentNames = new List<string>();
+            //var studentGrades = new List<int>();
 
             var adding = true;
 
             while (adding)
             {
+                var newStudent = new Student();
+                
                 Console.Write("Name: ");
-                studentNames.Add(Console.ReadLine());
+                newStudent.Name = (Console.ReadLine());
 
                 Console.Write("Grade: ");
-                studentGrades.Add(int.Parse(Console.ReadLine()));
+                newStudent.Grade = (int.Parse(Console.ReadLine()));
+                
+                Console.Write("Birthday: ");
+                newStudent.Birthday = (Console.ReadLine());
+
+                Console.Write("Address: ");
+                newStudent.Address = (Console.ReadLine());
+                
+                Console.Write("Phone Number: ");
+                newStudent.Phone = (Int64.Parse(Console.ReadLine()));
+
+                students.Add(newStudent);
 
                 Console.Write("Add another? y/n ");
                 if (Console.ReadLine().ToLower() != "y")
@@ -39,15 +55,30 @@ namespace SchoolTracker
             //    studentGrades[i] = int.Parse(Console.ReadLine());
             //}
 
-            for (int i = 0; i < studentNames.Count; i++)
+            foreach (var student in students)
             {
-                Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+                Console.WriteLine("Name: {0}, Grade: {1}", student.Name, student.Grade);
             }
+
+            // *** Replaced for loop below with foreach above when moving to List of Student objects
+            //for (int i = 0; i < studentNames.Count; i++)
+            //{
+            //    Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+            //}
 
             //foreach (var student in studentGrades)
             //{
             //    Console.WriteLine(student);
             //}
         }
+    }
+    
+    class Student
+    {
+        public string Name;
+        public int Grade;
+        public string Birthday;
+        public string Address;
+        public long Phone;
     }
 }
