b38e6258362b3f2763415d7a9542a2ea13ef9cd5
Adam Ayd <adam.ayd@gmail.com>
2018-11-15 22:48:25 -0500

    added school select for export using enums

--- Learning_CSharp/SchoolTracker/SchoolTracker/Program.cs
+++ Learning_CSharp/SchoolTracker/SchoolTracker/Program.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 
 namespace SchoolTracker
 {
+    enum EnumSchool
+    {
+        Hogwarts,
+        Harvard,
+        MIT
+    }
+
     class Program
     {
+        static List<Student> students = new List<Student>();
+
         static void Main(string[] args)
         {
-            //Console.Write("How many students in your class? ");
-            //var studentCount = int.Parse(Console.ReadLine());
-
-            var students = new List<Student>();
-
-            // replaced Lists below with List of Objects above
-            //var studentNames = new List<string>();
-            //var studentGrades = new List<int>();
-
             var adding = true;
 
             while (adding)
@@ -27,6 +27,8 @@ namespace SchoolTracker
                     newStudent.Name = Util.Console.Ask("Name: ");
 
                     newStudent.Grade = Util.Console.AskInt("Grade: ");
+
+                    newStudent.School = (EnumSchool) Util.Console.AskInt("School (type the corresponding number): \n  0: Hogwarts\n  1: Harvard\n  2: MIT\n");
 
                     newStudent.Birthday = Util.Console.Ask("Birthday: ");
 
@@ -56,38 +58,39 @@ namespace SchoolTracker
                 }
             }
 
-            //for (int i = 0; i < studentCount; i++)
-            //{
-            //    Console.Write("Name: ");
-            //    studentNames[i] = Console.ReadLine();
-
-            //    Console.Write("Grade: ");
-            //    studentGrades[i] = int.Parse(Console.ReadLine());
-            //}
-
             foreach (var student in students)
             {
                 Console.WriteLine("Name: {0}, Grade: {1}", student.Name, student.Grade);
             }
 
-            Console.WriteLine("{0} students", Student.Count);
+            Console.WriteLine(Student.Count + (Student.Count == 1 ? " student" : " students"));
 
-            // *** Replaced for loop below with foreach above when moving to List of Student objects
-            //for (int i = 0; i < studentNames.Count; i++)
-            //{
-            //    Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
-            //}
-
-            //foreach (var student in studentGrades)
-            //{
-            //    Console.WriteLine(student);
-            //}
+            Exports();
         }
 
         static void Import()
         {
             var importedStudent = new Student("Jack", 62, "Oct 17", "123 Main Street", 5551234567);
             Console.WriteLine(importedStudent.Name);
+        }
+
+        static void Exports()
+        {
+            foreach (var student in students)
+            {
+                switch (student.School)
+                {
+                    case EnumSchool.Hogwarts:
+                        Console.WriteLine("Exporting to Hogwarts");
+                        break;
+                    case EnumSchool.Harvard:
+                        Console.WriteLine("Exporting to Harvard");
+                        break;
+                    case EnumSchool.MIT:
+                        Console.WriteLine("Exporting to MIT");
+                        break;
+                }
+            }
         }
     }
 
@@ -109,6 +112,7 @@ namespace SchoolTracker
 
         public int Grade;
         public string Birthday;
+        public EnumSchool School;
 
         public Student()
         {
