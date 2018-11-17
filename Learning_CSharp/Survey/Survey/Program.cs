341d222c03b20bdf50ec2dcea8fe504fc99e30c7
Adam Ayd <adam.ayd@gmail.com>
2018-11-16 22:20:37 -0500

    moved zodiac signs to separate class

--- Learning_CSharp/Survey/Survey/Program.cs
+++ Learning_CSharp/Survey/Survey/Program.cs
@@ -19,45 +19,8 @@ namespace Survey
 
             answers.Display();
 
-            switch (answers.BirthMonth.ToLower())
-            {
-                case "january":
-                    Console.WriteLine("You are a Capricorn");
-                    break;
-                case "february":
-                    Console.WriteLine("You are an Aquarius");
-                    break;
-                case "march":
-                    Console.WriteLine("You are a Pices");
-                    break;
-                case "april":
-                    Console.WriteLine("You are an Aries");
-                    break;
-                case "may":
-                    Console.WriteLine("You are a Taurus");
-                    break;
-                case "june":
-                    Console.WriteLine("You are a Gemini");
-                    break;
-                case "july":
-                    Console.WriteLine("You are a Cancer");
-                    break;
-                case "august":
-                    Console.WriteLine("You are a Leo");
-                    break;
-                case "september":
-                    Console.WriteLine("You are a Virgo");
-                    break;
-                case "october":
-                    Console.WriteLine("You are a Libra");
-                    break;
-                case "november":
-                    Console.WriteLine("You are a Scorpio");
-                    break;
-                default:
-                    Console.WriteLine("You are a Sagittarius");
-                    break;
-            }
+            ZodiacSign zodiac = new ZodiacSign();
+            zodiac.FindZodiac(answers.BirthMonth);
         }
 
         static string TryAnswer()
---
+++ Learning_CSharp/Survey/Survey/ZodiacSign.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Survey
+{
+    public class ZodiacSign
+    {
+        public void FindZodiac(string month)
+        {
+            switch (month.ToLower())
+            {
+                case "january":
+                    Console.WriteLine("You are a Capricorn");
+                    break;
+                case "february":
+                    Console.WriteLine("You are an Aquarius");
+                    break;
+                case "march":
+                    Console.WriteLine("You are a Pices");
+                    break;
+                case "april":
+                    Console.WriteLine("You are an Aries");
+                    break;
+                case "may":
+                    Console.WriteLine("You are a Taurus");
+                    break;
+                case "june":
+                    Console.WriteLine("You are a Gemini");
+                    break;
+                case "july":
+                    Console.WriteLine("You are a Cancer");
+                    break;
+                case "august":
+                    Console.WriteLine("You are a Leo");
+                    break;
+                case "september":
+                    Console.WriteLine("You are a Virgo");
+                    break;
+                case "october":
+                    Console.WriteLine("You are a Libra");
+                    break;
+                case "november":
+                    Console.WriteLine("You are a Scorpio");
+                    break;
+                case "december":
+                    Console.WriteLine("You are a Sagittarius");
+                    break;
+            }
+
+        }
+    }
+}
