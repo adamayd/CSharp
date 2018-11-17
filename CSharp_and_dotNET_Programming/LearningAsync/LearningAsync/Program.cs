42e49a5bb0f1522d090812a19bf204d8116c8225
Adam Ayd <adam.ayd@gmail.com>
2018-11-17 14:11:56 -0500

    added async programming example solutions

---
+++ CSharp_and_dotNET_Programming/LearningAsync/LearningAsync/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsync
+{
+    class Program
+    {
+        public static String s = "";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading File");
+            Download();
+            s = Console.ReadLine();
+            Thread.Sleep(3000);
+        }
+
+        static void Download()
+        {
+            Task.Run(() => {
+                Thread.Sleep(3000);
+                Console.WriteLine("Download Complete");
+                Console.WriteLine(s);
+            });
+        }
+    }
+}
---
+++ CSharp_and_dotNET_Programming/LearningAsyncAwayStart/LearningAsyncAwayStart/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsyncAwayStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("downloading file");
+            Download();       
+            Console.ReadLine();
+
+        }
+
+        static async void Download()
+        {
+            HttpClient client = new HttpClient();
+
+            var data = await client.GetStringAsync("http://rouxacademy.com");
+            Console.WriteLine("Download Complete" + data);
+        }
+   }
+
+    //// Imaginary external network library
+
+    //class Network
+    //{
+    //    static public Task Download()
+    //    {
+    //        return Task.Run(() => Thread.Sleep(3000));
+    //    }
+    //}
+}
