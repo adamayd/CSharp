e04e68cb459fbebecb0859d69204d619757118ec
Adam Ayd <adam.ayd@gmail.com>
2018-11-17 20:53:11 -0500

    added solutions for properties examples

---
+++ CSharp_and_dotNET_Programming/LearningProperties/LearningProperties/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningProperties
+{
+    class Player
+    {
+        public bool IsAlive
+        {
+            get { return Health > 0; }
+        }
+
+        public int Health { get; private set; } = 100;
+
+        public void Hit()
+        {
+            Random r = new Random();
+            Health -= r.Next(5, 50);
+        }
+
+        //public void CheckDeath()
+        //{
+        //    if (health <= 0)
+        //        isAlive = false;
+        //}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                //player.CheckDeath();
+                Console.WriteLine("Is player alive: " + player.IsAlive);
+            }
+        }
+    }
+}
