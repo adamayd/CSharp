02faffa3677ca6562d237a0737e1ff323ed9176d
Adam Ayd <adam.ayd@gmail.com>
2018-11-17 22:37:02 -0500

    solutions to using statement examples - course complete

---
+++ CSharp_and_dotNET_Programming/LearningUsing/LearningUsing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningUsing
+{
+    class MyResource : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Disposing Resource");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var resource = new MyResource())
+            {
+                Console.WriteLine("Using Resource");
+            }
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
