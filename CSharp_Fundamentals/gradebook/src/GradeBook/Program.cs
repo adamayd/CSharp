362cee22e88f89d95fbe3ab6be0ddc0097af98c0
Adam Ayd <aayd@hm1842.com>
2019-07-20 23:14:05 -0400

    completed chapter 3

--- CSharp_Fundamentals/gradebook/src/GradeBook/Program.cs
+++ CSharp_Fundamentals/gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,11 +7,24 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var grades = new List<double>() { 12.5, 33.2, 45.1, 4.7};
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(var number in grades) {
+                result += number;
+            }
+            
+            result /= grades.Count;
+
+            Console.WriteLine($"The average grade is {result:N1}");
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]}!");
             }
-            else {
+            else
+            {
                 Console.WriteLine("Hello World!");
             }
         }
